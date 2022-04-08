36c1e07903fd89efa9441a8190736e44c123d137
szkolakowski <szymon.kolakowski@gmail.com>
2022-04-08 14:10:47 +0200

    auto update

---
+++ firstScripts/04_arithmeticoperators.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val a = 5
+    val b = 3.0
+    println("a=" + a + " b=" + b)
+    println("a+b=${a + b}")
+    println("a-b=${a - b}")
+    println("a/b=${a / b}")
+    println("a*b=${a * b}")
+    println("a%b=${a % b}")
+}
