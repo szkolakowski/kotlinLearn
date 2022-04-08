0afe016848a7f1f5120e9ff45b6a9837ee1346c5
szkolakowski <szymon.kolakowski@gmail.com>
2022-04-08 14:19:26 +0200

    auto update

--- firstScripts/05_arrays.kt
+++ firstScripts/05_arrays.kt
@@ -4,5 +4,10 @@ fun main() {
     println(names.size)
     println(names[0])
     names[0] = "Jeremy"
-    println(names[0])
+    println(names[0][0])
+    println(names[0].length)
+    names.set(0, "James")
+    println("number of elements: ${names.size}")
+    println("first name: ${names[0]}")
+    println("last name: ${names[names.size - 1]}")
 }
