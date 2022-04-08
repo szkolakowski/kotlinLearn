75ab9dcae050a4765f4260fb3a88554f0473057f
szkolakowski <szymon.kolakowski@gmail.com>
2022-04-08 14:08:47 +0200

    auto update

---
+++ firstScripts/03_basicdatatypes.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val exampleString = "example" // String
+    val exampleInt = 200 // Int
+    val exampleLong = 20000000000 // Long
+    val exampleChar = 'e' // Char
+    val exampleBoolean = true // Boolean
+    // string is not a primitive type :)
+    // there are also: Byte, Short, Float, Double
+}
