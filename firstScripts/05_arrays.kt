1eea1accc4e356fac690e43d9ed617cf7ef29af3
szkolakowski <szymon.kolakowski@gmail.com>
2022-04-08 14:15:59 +0200

    auto update

---
+++ firstScripts/05_arrays.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val names = arrayOf("Jim", "John", "Jenny")
+    println(names.indices)
+    println(names.size)
+    println(names[0])
+    names[0] = "Jeremy"
+    println(names[0])
+}
